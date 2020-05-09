548d3bbb34bc458e0c55f522291a31434371e2e2
Win <winzaza@msn.com>
2020-05-09 15:58:50 +0700

    fix path incorrect

--- script-travis.sh
+++ script-travis.sh
@@ -8,7 +8,7 @@ docker-compose -f docker-compose-test.yml exec nginx sh -c "nc -z -v backend_app
 docker-compose -f docker-compose-test.yml exec backend_app sh -c "ping  -c 4 nginx"
 docker-compose -f docker-compose-test.yml exec backend_app sh -c "ping  -c 4 mysql"
 docker-compose -f docker-compose-test.yml exec backend_app sh -c "nc -z -v mysql 3306"
-docker-compose -f docker-compose-test.yml logs node_app_test
+docker-compose -f docker-compose-test.yml logs backend_app
 # curl -s http://localhost:8000/test --connect-timeout 20  --progress-bar | cat # output json write data in mysql
 while ! curl -s http://localhost:80/health --connect-timeout 20  --progress-bar ; do sleep 1 ; done
 # docker exec -it node_mariadb_test sh -c "mysql -u root -ptest -e 'SHOW DATABASES; USE db_assign; SELECT * from user_knex;'"
